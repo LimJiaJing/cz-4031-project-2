6a0090ae16df555bbe492ddf401be2d6d2f4e90a
MIBN21 <92128168+MIBN21@users.noreply.github.com>
2022-10-24 23:44:46 +0800

    Add files via upload

---
+++ Table SQL Creation/region.sql
@@ -0,0 +1,19 @@
+-- Table: public.region
+
+-- DROP TABLE IF EXISTS public.region;
+
+CREATE TABLE public.region
+(
+	r_regionkey integer NOT NULL,
+	r_name character(25) COLLATE pg_catalog."default" NOT NULL,
+	r_comment character varying(152) COLLATE pg_catalog."default",
+	CONSTRAINT region_pkey PRIMARY KEY(r_regionkey)
+)
+WITH(
+	OIDS = FALSE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.region
+    OWNER to postgres;
