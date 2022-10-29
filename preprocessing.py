04d1311c052d924a25a848332582507152e8751b
MIBN21 <92128168+MIBN21@users.noreply.github.com>
2022-10-29 21:55:17 +0800

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -23,17 +23,16 @@ def connect():
     #https://www.folkstalk.com/2022/09/postgresql-show-tables-with-code-examples.html
     #this one to fetch the tables we created
     cur.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
-    list_tables = list(map(lambda x: x[0], cur.fetchall()))
-    print(list_tables)
+    list_of_tables = list(map(lambda x: x[0], cur.fetchall()))
+    print(list_of_tables)
     print("\n\n\n")
-    table_dict = {}
     ##fetch the columns of the tables
-    table_dict = {}
-    for i in list_tables:
+    table_cols = {}
+    for i in list_of_tables:
         cur.execute("SELECT column_name FROM information_schema.columns WHERE TABLE_NAME = '{}';".format(i))
         column = list(map(lambda x: x[0], cur.fetchall()))
-        table_dict[i] = column
-    return conn, table_dict
+        table_cols[i] = column
+    return conn, table_cols
 
 if __name__ == "__main__" :
     print("okay")
