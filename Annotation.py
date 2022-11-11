2a56a8c83667cae35057480615c1cbc78d8cf180
Mist0919 <XUEY0013@e.ntu.edu.sg>
2022-11-11 16:24:48 +0800

    Add annotation(test) and change preprocessing

--- Annotation.py
+++ Annotation.py
@@ -1,10 +1,83 @@
+#Test
+
 from treelib import Node, Tree
+from deepdiff import DeepDiff
 import json
+import re
+import preprossessing
+
+def bfs(root):
+    queue = []
+    queue.append(root)
+    while len(queue) > 0:
+        cur_node = queue.pop()
+        print("Node:", cur_node.node_type)
+        if "Relation Name" in cur_node.attributes:
+            print("Relation Name:", cur_node.attributes["Relation Name"])
+        for child in cur_node.children:
+            queue.append(child)
 
 def read_json(path):
     f = open(path)
     res = json.load(f)
     return res;
+
+def parse_cond(raw):
+    raw = re.sub(r'(.*\.)?(.*) = (.*\.)?(.*)', r'\2 = \4', raw)
+    raw = re.sub(r"(.*\.)?(.*) = ('.*')(::.*)", r'\2 = \3', raw)
+    raw = re.sub(r"(.*\.)?(.*) (>=|<=|=) ('.*')(::.*)", r'\2 <= \4', raw)
+    raw = re.sub(r"(.*\.)?(.*)(::.*) ~~ ('%.*')(::.*)", r'\2 like \4', raw)
+    return raw
+
+def test_parsing(tree):
+    join_cond = ["Merge Cond", "Hash Cond", "Index Cond"]
+    cond_dic = {"Merge Cond": "Merge Join", "Hash Cond": "Hash Join", "Index Cond": "Index-based Join"}
+    scans = ["Seq Scan", "Index Scan", "Bitmap Scan", "Index Only Scan"]
+    for node in tree.all_nodes():
+        res = None
+        find_conds = [c for c in join_cond if c in node.data.keys()]
+        if len(find_conds) > 0:
+            cond = find_conds[0]
+            temp_cond = node.data[cond].replace("(", "").replace(")", "")
+            for subcond in temp_cond.split(" AND "):
+                for subcond1 in subcond.split(" OR "):
+                    res = " ".join((cond_dic[cond], "join condition:", parse_cond(subcond), "cost:", str(node.data["Startup Cost"])))
+        elif node.tag in scans:
+            res = " ".join((node.tag, "on table", node.data["Relation Name"], "cost:", str(node.data["Startup Cost"])))
+        if res:
+            print(res)
+
+def compare(tree1, tree2):
+    # l1 = [node.tag for node in tree1.all_nodes()]
+    # l2 = [node.tag for node in tree2.all_nodes()]
+    # l2.remove("Hash")
+    # print(l1)
+    # print(l2)
+    # d = DeepDiff(l1, l2)
+    # print(d["iterable_item_removed"])
+
+
+    print("---------------------------------------------------------------------")
+    for node1 in tree2.all_nodes():
+        res = []
+        for cond in join_cond:
+            if cond in node1.data.keys():
+                temp_cond = node1.data[cond].replace("(", "").replace(")", "")
+                for subcond in temp_cond.split(" AND "):
+                    res.append((node1.tag, cond, "condition:", parse_cond(subcond)))
+        if len(res) > 0:
+            print(node1.tag, cond, "condition:",res)
+        # if "Filter" in node1.data.keys():
+        #     node2 = [node.tag for node in tree2.all_nodes() if "Filter" in node.data.keys() and
+        #              node.data["Filter"].find(node1.data["Filter"]) != -1]
+        #     print("Scan approach: QEP",node1.tag, "AQP", node2[0])
+        #     print(node1.data["Filter"])
+        # if "Hash Cond" in node1.data.keys():
+        #     node2 = [node.tag for node in tree2.all_nodes() if "Hash Cond" in node.data.keys() and
+        #              node.data["Hash Cond"].find(node1.data["Hash Cond"]) != -1]
+        #     print("Hash join approach: QEP:",node1.tag, "AQP:", node2[0])
+        #     print(node1.data["Hash Cond"])
+
 
 def generate_operation_tree(plan):
     tree = Tree()
@@ -12,7 +85,7 @@ def generate_operation_tree(plan):
     queue = []
     node_id = 0
     queue.append((plan, node_id))
-    tree.create_node(plan["Node Type"], node_id)
+    tree.create_node(plan["Node Type"], node_id, data = get_attributes(plan))
     node_id += 1
 
     while len(queue) > 0:
@@ -22,13 +95,45 @@ def generate_operation_tree(plan):
         if "Plans" in  cur_plan.keys():
             for subplan in cur_plan["Plans"]:
                 queue.append((subplan, node_id))
-                tree.create_node(subplan["Node Type"], node_id, parent = cur_plan_id)
+                tag = subplan["Node Type"]
+                if subplan["Parent Relationship"] == "SubPlan":
+                    tag = tag + "(SubPlan)"
+                tree.create_node(tag, node_id, data = get_attributes(subplan), parent = cur_plan_id)
                 node_id += 1
 
     return tree
 
+def get_attributes(plan):
+    attributes = {}
+    for key, value in plan.items():
+        if not key == "Plans" and not key == "Node Type":
+            attributes[key] = value
+    return attributes
 
-test = read_json("temp.json")
-print(test)
-tree = generate_operation_tree(test[0][0][0]["Plan"])
-tree.show()
+if __name__ == "__main__":
+    start = preprossessing.connect()
+    conn = start[0]
+    tables = start[1]
+    # print(tables)
+    query_string = preprossessing.query_asker()
+    preprossessing.QEP_Generator(conn, query_string)
+    preprossessing.AQP_generator(conn, query_string)
+    print("?")
+    test = read_json("QEP.json")
+    test1 = read_json("AQP1.json")
+    print(test)
+    tree1 = generate_operation_tree(test[0][0][0]["Plan"])
+    tree1.show()
+    tree2 = generate_operation_tree(test1[0][0][0]["Plan"])
+    tree2.show()
+    test_parsing(tree1)
+    print("---------------------------------------------")
+    test_parsing(tree2)
+    str = "a = b"
+    str = str.split(" = ")
+    print(str)
+    raw = "lineitem.l_shipdate <= '1998:0803000000'::time stamp without time zone"
+    raw = re.sub(r"(\w+\.)?(\w+) (>=|<=|=) ('.*')(::.*)", r'\2 <= \4', raw)
+    print("test",raw)
+    # for node in tree2.all_nodes():
+    #     print(node.tag, node.identifier)
--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,13 @@ import psycopg2
 import sys
 import json
 
+
 def connect():
-    #login instructions
-    username="postgres"
-    password="cz4031group34"
-    database="TPC-H"
-    login_data= f"dbname={database} user={username} password={password}"
+    # login instructions
+    username = "postgres"
+    password = "cz4031group34"
+    database = "TPC-H"
+    login_data = f"dbname={database} user={username} password={password}"
     conn = None
 
     print('Connecting to the PostgreSQL database...')
@@ -16,13 +17,14 @@ def connect():
     print('Connected')
     print("\n\n")
     cur = conn.cursor()
-    #https://support.labs.cognitiveclass.ai/knowledgebase/articles/835206-access-postgresql-from-python-using-pscopg2
+    # https://support.labs.cognitiveclass.ai/knowledgebase/articles/835206-access-postgresql-from-python-using-pscopg2
     cur.execute("SELECT datname FROM pg_database;")
     # list all the databses in our account
     list_database = cur.fetchall()
-    #https://www.folkstalk.com/2022/09/postgresql-show-tables-with-code-examples.html
-    #this one to fetch the tables we created
-    cur.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
+    # https://www.folkstalk.com/2022/09/postgresql-show-tables-with-code-examples.html
+    # this one to fetch the tables we created
+    cur.execute(
+        "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
     list_of_tables = list(map(lambda x: x[0], cur.fetchall()))
     ##fetch the columns of the tables
     table_cols = {}
@@ -32,15 +34,17 @@ def connect():
         table_cols[i] = column
     return conn, table_cols
 
+
 def query_asker():
-    #first ask for input
+    # first ask for input
     # append Explain (from json) to front of input string
     print("Key in your Query. At the end of your query, press enter, Key in Ctrl + Z and then press enter again.\n")
     query = sys.stdin.read()
-    modified_query= "Explain (format JSON) " + query
+    modified_query = "SET max_parallel_workers_per_gather = 0;EXPLAIN (FORMAT JSON, ANALYZE, VERBOSE) "  + query
     return modified_query
 
-def QEP_Generator(database_conn,input_string):
+
+def QEP_Generator(database_conn, input_string):
     cursor = database_conn.cursor()
     cursor.execute(input_string)
     data = cursor.fetchall()
@@ -49,26 +53,28 @@ def QEP_Generator(database_conn,input_string):
     with open("QEP.json", "w") as outfile:
         outfile.write(json_object)
     print("QEP is generated in QEP.json")
-    
+
+
 def generate_operation_list(plan):
     plan_list = []
     plan_list.append(plan)
-    tlist=[plan["Node Type"]]
+    tlist = [plan["Node Type"]]
 
     while len(plan_list) > 0:
         node = plan_list.pop()
         cur_plan = node
-        if "Plans" in  cur_plan.keys():
+        if "Plans" in cur_plan.keys():
             for subplan in cur_plan["Plans"]:
                 plan_list.append(subplan)
                 tlist.append(subplan["Node Type"])
     return tlist
 
-def conditions_generator(operation_list,input_string):
-    #The following was not added as of yet
-    #enable_async_append (boolean)
-    # enable_incremental_sort (boolean) 
-    # enable_tidscan (boolean) 
+
+def conditions_generator(operation_list, input_string):
+    # The following was not added as of yet
+    # enable_async_append (boolean)
+    # enable_incremental_sort (boolean)
+    # enable_tidscan (boolean)
     # enable_parallel_append (boolean)
     # enable_parallel_hash (boolean)
     # enable_partition_pruning (boolean)
@@ -78,10 +84,10 @@ def conditions_generator(operation_list,input_string):
     list_of_AQP_Queries = []
     list_of_disables = []
     count = 1
-    temp_input_string = "SET max_parallel_workers_per_gather = 0;\n"+input_string
+    temp_input_string = "SET max_parallel_workers_per_gather = 0;\n" + input_string
     for i in operation_list:
-        #Gather not in because i dont know where to pu
-        #not sure about hash/aggregate
+        # Gather not in because i dont know where to pu
+        # not sure about hash/aggregate
         # if i == "Hash" or i == "Aggregate":
         #     modified_string="SET enable_hashagg TO off;\n"+temp_input_string
         #     if modified_string not in list_of_AQP_Queries:
@@ -89,17 +95,17 @@ def conditions_generator(operation_list,input_string):
         #       list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
         #       count+=1
         if i == "Hash Join":
-            modified_string="SET enable_hashjoin TO off;\n"+temp_input_string
+            modified_string = "SET enable_hashjoin TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         elif i == "Bitmap Heap Scan" or i == "Bitmap Index Scan":
-            modified_string="SET enable_bitmapscan TO off;\n"+temp_input_string
+            modified_string = "SET enable_bitmapscan TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         # elif i == "Seq Scan":
         #     modified_string="SET enable_seqscan TO off;\n"+temp_input_string
         #     if modified_string not in list_of_AQP_Queries:
@@ -107,17 +113,17 @@ def conditions_generator(operation_list,input_string):
         #       list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
         #       count+=1
         elif i == "Index Only Scan":
-            modified_string="SET enable_indexonlyscan TO off;\n"+temp_input_string
+            modified_string = "SET enable_indexonlyscan TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         elif i == "Index Scan":
-            modified_string="SET enable_indexscan TO off;\n"+temp_input_string
+            modified_string = "SET enable_indexscan TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         # elif i == "Materialize":
         #     modified_string="SET enable_material TO off;\n"+temp_input_string
         #     if modified_string not in list_of_AQP_Queries:
@@ -131,24 +137,24 @@ def conditions_generator(operation_list,input_string):
         #       list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
         #       count+=1
         elif i == "Nested Loop":
-            modified_string="SET enable_nestloop TO off;\n"+temp_input_string
+            modified_string = "SET enable_nestloop TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
-        #not sure about gather merge
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
+        # not sure about gather merge
         elif i == "Merge Join":
-            modified_string="SET enable_mergejoin TO off;\n"+temp_input_string
+            modified_string = "SET enable_mergejoin TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         elif i == "Gather Merge":
-            modified_string = "SET enable_gathermerge TO off;\n"+temp_input_string
+            modified_string = "SET enable_gathermerge TO off;\n" + temp_input_string
             if modified_string not in list_of_AQP_Queries:
                 list_of_AQP_Queries.append(modified_string)
-                list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
-                count+=1
+                list_of_disables.append("AQP{0} has {1} disabled\n".format(count, i))
+                count += 1
         # elif i == "Memoize":
         #     modified_string = "SET enable_memoize TO off;\n"+temp_input_string
         #     if modified_string not in list_of_AQP_Queries:
@@ -161,29 +167,29 @@ def conditions_generator(operation_list,input_string):
             my_data_file.write(list_of_disables[i])
     return list_of_AQP_Queries
 
-def AQP_generator(database_conn,input_string):
+
+def AQP_generator(database_conn, input_string):
     with open("QEP.json") as my_data_file:
         my_data = json.load(my_data_file)
     operation_list = generate_operation_list(my_data[0][0][0]["Plan"])
-    list_of_AQP_Queries = conditions_generator(operation_list,input_string)
+    list_of_AQP_Queries = conditions_generator(operation_list, input_string)
     for i in range(len(list_of_AQP_Queries)):
         cursor = database_conn.cursor()
         cursor.execute(list_of_AQP_Queries[i])
         data = cursor.fetchall()
         json_object = json.dumps(data, indent=4)
         # Writing to sample.json
-        with open("AQP" + "{}.json".format(i+1), "w") as outfile:
+        with open("AQP" + "{}.json".format(i + 1), "w") as outfile:
             outfile.write(json_object)
-            print("AQP(s) is generated in AQP{}.json".format(i+1))
+            print("AQP(s) is generated in AQP{}.json".format(i + 1))
 
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     # print("okay")
     start = connect()
     conn = start[0]
     tables = start[1]
     # print(tables)
     query_string = query_asker()
-    QEP_Generator(conn,query_string)
-    AQP_generator(conn,query_string)
+    QEP_Generator(conn, query_string)
+    AQP_generator(conn, query_string)
