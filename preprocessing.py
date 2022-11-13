173b0df66f2b198bd9146145c22d967d4d3f5f71
LimJiaJing <jiajinglim06.study@gmail.com>
2022-11-13 13:37:55 +0800

    merge database connection, plan generation, and json cleaning

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,31 @@
 import psycopg2
 import sys
 import json
+import os
+import re
 
+QEP_FILENAME = "qep.json"
+PLANS_DIRECTORY = "generated_plans"
+JSON_KEYS_TO_KEEP = [
+    "Plans",
+    "Total Cost",
+    "Node Type",
+    "Relation Name",
+    "Alias",
+    "Filter",
+    "Hash Cond",
+    "Merge Cond",
+    "Index Cond",
+    "Join Filter",
+    "Index Name",
+]
+JOIN_CONDS = [
+    "Hash Cond",
+    "Merge Cond",
+    "Index Cond",
+    "Join Filter",
+]
+FILTERS = ["Filter", "Join Filter"]
 
 def connect():
     # login instructions
@@ -36,23 +60,31 @@ def connect():
 
 
 def query_asker():
-    # first ask for input
-    # append Explain (from json) to front of input string
-    print("Key in your Query. At the end of your query, press enter, Key in Ctrl + Z and then press enter again.\n")
-    query = sys.stdin.read()
-    modified_query = "SET max_parallel_workers_per_gather = 0;EXPLAIN (FORMAT JSON, ANALYZE, VERBOSE) "  + query
+    query = ""
+    print("Please key in your Query:")
+    while True:
+        newline = input().rstrip()
+        if newline:
+            query = f"{query}\n{newline}"
+        else:
+            break
+    print("Finished reading query.\n")
+    print("Generating QEP and AQP(s).\n")
+    modified_query = "SET max_parallel_workers_per_gather = 0;EXPLAIN (FORMAT JSON, ANALYZE, VERBOSE) " + query
     return modified_query
 
 
-def QEP_Generator(database_conn, input_string):
+def qep_generator(database_conn, query):
     cursor = database_conn.cursor()
-    cursor.execute(input_string)
+    cursor.execute(query)
     data = cursor.fetchall()
     json_object = json.dumps(data, indent=4)
     # Writing to sample.json
-    with open("QEP.json", "w") as outfile:
-        outfile.write(json_object)
-    print("QEP is generated in QEP.json")
+    create_dir(PLANS_DIRECTORY)
+    qep_path = os.path.join(PLANS_DIRECTORY, QEP_FILENAME)
+    with open(qep_path, "w") as f:
+        f.write(json_object)
+    print(f"QEP is generated and written to {qep_path}.")
 
 
 def generate_operation_list(plan):
@@ -86,6 +118,7 @@ def conditions_generator(operation_list, input_string):
     count = 1
     temp_input_string = "SET max_parallel_workers_per_gather = 0;\n" + input_string
     for i in operation_list:
+        print(i)
         # Gather not in because i dont know where to pu
         # not sure about hash/aggregate
         # if i == "Hash" or i == "Aggregate":
@@ -161,35 +194,134 @@ def conditions_generator(operation_list, input_string):
         #       list_of_AQP_Queries.append(modified_string)
         #       list_of_disables.append("AQP{0} has {1} disabled\n".format(count,i))
         #       count+=1
-        print(temp_input_string)
-    with open("AQPInfo.txt", "w") as my_data_file:
+        # print(temp_input_string)
+    aqp_info_filename = "aqp_info.txt"
+    aqp_info_path = os.path.join(PLANS_DIRECTORY, aqp_info_filename)
+    with open(aqp_info_path, "w") as f:
         for i in range(len(list_of_disables)):
-            my_data_file.write(list_of_disables[i])
+            f.write(list_of_disables[i])
     return list_of_AQP_Queries
 
 
-def AQP_generator(database_conn, input_string):
-    with open("QEP.json") as my_data_file:
-        my_data = json.load(my_data_file)
+def aqp_generator(database_conn, query):
+    qep_path = os.path.join(PLANS_DIRECTORY, QEP_FILENAME)
+    with open(qep_path) as f:
+        my_data = json.load(f)
     operation_list = generate_operation_list(my_data[0][0][0]["Plan"])
-    list_of_AQP_Queries = conditions_generator(operation_list, input_string)
-    for i in range(len(list_of_AQP_Queries)):
+    aqps = conditions_generator(operation_list, query)
+    print(aqps)
+    for i in range(len(aqps)):
         cursor = database_conn.cursor()
-        cursor.execute(list_of_AQP_Queries[i])
+        cursor.execute(aqps[i])
         data = cursor.fetchall()
         json_object = json.dumps(data, indent=4)
-        # Writing to sample.json
-        with open("AQP" + "{}.json".format(i + 1), "w") as outfile:
-            outfile.write(json_object)
-            print("AQP(s) is generated in AQP{}.json".format(i + 1))
+
+        # output to file
+        aqp_filename = f"aqp_{i+1}.json"
+        aqp_path = os.path.join(PLANS_DIRECTORY, aqp_filename)
+        with open(aqp_filename, "w") as f:
+            f.write(json_object)
+            print(f"AQP {i+1} is generated and written to {aqp_path}")
 
 
-if __name__ == "__main__":
-    # print("okay")
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def read_json(path):
+    f = open(path)
+    res = json.load(f)
+    return res;
+
+
+def clean_plan(plan):
+
+    if "Plans" in list(plan):
+        plans = plan["Plans"]
+        for subplan in plans:
+            clean_plan(subplan)
+    for key in list(plan):
+        # remove unneeded keys from plan
+        if key not in JSON_KEYS_TO_KEEP:
+            plan.pop(key, None)
+        else:
+            if key in JOIN_CONDS + FILTERS:
+                plan[key] = clean_cond(plan[key])
+
+
+def clean_cond(raw):
+    cond = raw.replace("(", "").replace(")", "")
+    cond = cond.split(" AND ")
+    for i in range(len(cond)):
+        cond[i] = cond[i].split(" OR ")
+    clean = ""
+    for OR_list in cond:
+        if len(clean) != 0:
+            clean = clean + " AND"
+        for temp_cond in OR_list:
+            temp_cond = parse_cond(temp_cond.strip())
+            if len(clean) == 0:
+                clean = temp_cond
+            elif clean[-3:] == "AND":
+                clean = clean + " " + temp_cond
+            else:
+                clean = clean + " OR " + temp_cond
+    return clean
+
+
+def parse_cond(raw):
+    # need to create condition for if there is decimal
+    raw = re.sub(r'([a-z].*\.)?(.*) (>=|<=|=|<|>|<>) ([a-z].*\.)?(.*)', r'\2 \3 \5', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*) (>=|<=|=|<|>|<>) (.*)(::.*)", r'\2 \3 \4', raw)
+    raw = re.sub(r"([a-z]...*\.)?(.*)(::.*) (>=|<=|=|<|>|<>) (.*)", r'\2 \4 \5', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*)(::.*) (>=|<=|=|<|>|<>) (.*)(::.*)", r'\2 \4 \5', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*)(::.*) ~~ ('.*')(::.*)", r'\2 like \4', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*)(::.*) !~~ ('.*')(::.*)", r'\2 not like \4', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*) ANY '({.*})'(::.*)", r'\3', raw)
+    raw = re.sub(r"([a-z].*\.)?(.*) ANY '({.*})'", r'\3', raw)
+    raw = re.sub(r"(.*) (>=|<=|=|<|>|<>) '([0-9]*)'", r'\1 \2 \3', raw)
+    raw = re.sub(r"(.*)(SubPlan [0-9])(.*)", r'\1_\3', raw)
+    return raw
+
+def clean_json_files():
+    json_filenames = [f for f in os.listdir(PLANS_DIRECTORY) if (os.path.isfile(os.path.join(PLANS_DIRECTORY, f)) and f[-4:] == "json")]
+    for json_filename in json_filenames:
+        json_path = os.path.join(PLANS_DIRECTORY, json_filename)
+        plan = read_json(json_path)[0][0][0]["Plan"]
+        clean_plan(plan)
+        json_object = json.dumps(plan, indent=4)
+        clean_filename = f"clean_{json_filename}"
+        clean_path = os.path.join(PLANS_DIRECTORY, clean_filename)
+        with open(clean_path, "w") as f:
+            f.write(json_object)
+            print(f"Clean {json_filename} is generated and written to {clean_path}")
+
+
+def run_preprocessing():
     start = connect()
     conn = start[0]
     tables = start[1]
     # print(tables)
-    query_string = query_asker()
-    QEP_Generator(conn, query_string)
-    #AQP_generator(conn, query_string)
+# <<<<<<< Updated upstream
+    query = query_asker()
+    qep_generator(conn, query)
+    aqp_generator(conn, query)
+
+    ## generate clean json files
+    clean_json_files()
+# =======
+#     # read all files
+#     sql_filenames = [f for f in listdir("Queries&Json") if (isfile(join("Queries&Json", f)) and f[-3:] == "sql")]
+#
+#     for sql_filename in sql_filenames:
+#         if sql_filename[-5:] == "e.sql":
+#             continue
+#         f = open("Queries&Json\\"+sql_filename, "r")
+#         sql_file = f.read()
+#         f.close()
+#         query_string = query_asker(sql_file)
+#         qep_generator(conn, query_string, sql_filename)
+#         # aqp_generator(conn, query_string)
+# >>>>>>> Stashed changes
+if __name__ == "__main__":
+    run_preprocessing()
